56f7587e554c963329e209ccbf3d6263b427bfd4
Rafael San José <rsanjose@alxarafe.com>
2026-02-16 08:01:19 +0100

    Ignore blade cache

--- var/cache/blade/03835f766d74e552700df1f36146ca9a.php
+++
@@ -1,9 +0,0 @@
-<div class="form-check">
-    <input type="hidden" name="<?php echo $name; ?>" value="0">
-    <input class="form-check-input" type="checkbox" name="<?php echo $name; ?>" value="1" id="<?php echo $name; ?>" <?php if($value): ?> checked <?php endif; ?>>
-    <label class="form-check-label" for="<?php echo $name; ?>">
-        <?php echo $label; ?>
-
-    </label>
-</div>
-<?php /**PATH /var/www/html/vendor/alxarafe/alxarafe/templates/form/boolean.blade.php ENDPATH**/ ?>
--- var/cache/blade/5d994174be3f855d127fef276b189449.php
+++
@@ -1,7 +0,0 @@
-    <div id="id-right">
-        <?php echo $__env->make('partial.user_menu', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-        
-        <?php echo $__env->make('partial.alerts', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-        <?php echo $__env->yieldContent('content'); ?>
-    </div>
-<?php /**PATH /var/www/html/templates/themes/alxarafe/partial/body_standard.blade.php ENDPATH**/ ?>
